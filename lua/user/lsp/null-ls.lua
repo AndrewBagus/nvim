09ccdffa1f99791fcfd85fc36670d0d155a423bb
Andrew Bagus <bagus.andrew@gmail.com>
2022-08-10 18:28:29 +0700

    update

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -13,7 +13,7 @@ null_ls.setup({
 	sources = {
 		formatting.prettier.with({
 			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-			extra_filetypes = { "php", "blade" },
+			extra_filetypes = { "php" },
 		}),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.phpcbf,
